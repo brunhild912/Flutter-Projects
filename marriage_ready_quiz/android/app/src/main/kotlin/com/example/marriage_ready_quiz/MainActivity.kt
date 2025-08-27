38aec211d741fc63bdce6bbc0a1221c8ab0a25e2
Ariba Anjum <92753897+brunhild912@users.noreply.github.com>
2025-08-27 21:30:11 +0500

    Delete marriage_ready_quiz directory

--- marriage_ready_quiz/android/app/src/main/kotlin/com/example/marriage_ready_quiz/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.marriage_ready_quiz
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- marriage_ready_quiz/lib/core/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-ThemeData buildTheme() {
-  final base = ThemeData(useMaterial3: true, colorSchemeSeed: Colors.teal);
-  return base.copyWith(
-    appBarTheme: const AppBarTheme(centerTitle: true),
-    visualDensity: VisualDensity.adaptivePlatformDensity,
-  );
-}
